90960e2758340c0d85e21394e4f65b9c81680d9e
Angelo Romito <angelo.romito@de.markant.com>
2022-01-31 06:22:57 +0100

    Initial commit

---
+++ app/src/main/java/de/trundicho/timeclockstamper/MainActivity.java
@@ -0,0 +1,46 @@
+package de.trundicho.timeclockstamper;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import de.trundicho.timeclockstamper.core.adapters.api.TimeClockStamperApi;
+import de.trundicho.timeclockstamper.core.adapters.api.TimeClockStamperApiImpl;
+import de.trundicho.timeclockstamper.core.adapters.persistence.FilePersistence;
+import de.trundicho.timeclockstamper.ui.main.SectionsPagerAdapter;
+import de.trundicho.timeclockstamper.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = binding.fab;
+
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+
+        TimeClockStamperApi clockStamperApi = new TimeClockStamperApiImpl("",
+                new FilePersistence("", "", ""));
+    }
+}
